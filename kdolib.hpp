d0521543977182b6d359a51c4bec1294f8c6572d
Nils Laeremans <nlaerema@c1r6p13.42lehavre.fr>
2024-02-18 20:48:20 +0100

    t_uint

--- kdolib.hpp
+++ kdolib.hpp
@@ -6,7 +6,7 @@
 /*   By: nlaerema <nlaerema@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 10:58:17 by nlaerema          #+#    #+#             */
-/*   Updated: 2024/02/18 17:28:22 by nlaerema         ###   ########.fr       */
+/*   Updated: 2024/02/18 20:47:54 by nlaerema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 # include <string>
 # include <iostream>
 # include "io/get_userInput.hpp"
+
+typedef unsigned int t_uint;
 
 #endif
